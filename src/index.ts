e0856e5db02367b48e99861062708b7f54060502
SimonFJ20 <simonfromjakobsen@gmail.com>
2021-02-22 14:38:16 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,65 @@
+
+/*
+*       Artika API Server
+*
+*       Filename:   index.ts
+*       Pathname:   ./index.ts
+*       Content:    Main entry point for server
+*
+*       License:    GPL-2.0
+*
+*       Author:     Simon From Jakobsen
+*       Email:      simonfromjakobsen@gmail.com
+*       GitHub:     SimonFJ20
+*
+*       Created:    22-02-2021
+*       Last Edit:  22-02-2021
+*/
+
+import cors from 'cors';
 import express from 'express';
+import 'dotenv/config';
 
 
 
-
-
+// init server
 const server = express();
-const port = 8000;
+
+// for easier deployment
+let port: number = 0;
+
+if(process.env.PORT != null) {
+    port = parseInt(<string> process.env.PORT);
+} else {
+    throw new Error('PORT not defined as ENV variable');
+}
+
+
+
+
+// for access
+server.use(cors());
+
+// using JSON as default
+server.use(express.json({type: 'application/json'}));
 
 
 
 
 
-
-
-
-
+// test
 server.get('/', (req, res) => {
     res.send('Bruh');
 });
 
 
 
-
+// run server
 server.listen(port, () => {
 
-    console.log(`Express on port`, port);
+    console.log(`Artika API server on port`, port);
 
 });
+
+
+
