92c414d9d22c6ecd7f5921e8c0644fd74d7c7d17
SimonFJ20 <simonfromjakobsen@gmail.com>
2021-02-22 14:04:40 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,29 @@
 import express from 'express';
-const app = express();
-const PORT = 8000;
-app.get('/', (req, res) => res.send('Express + TypeScript Server'));
-app.listen(PORT, () => {
-  console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
+
+
+
+
+
+const server = express();
+const port = 8000;
+
+
+
+
+
+
+
+
+
+server.get('/', (req, res) => {
+    res.send('Bruh');
+});
+
+
+
+
+server.listen(port, () => {
+
+    console.log(`Express on port`, port);
+
 });
