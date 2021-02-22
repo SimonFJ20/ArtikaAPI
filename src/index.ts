4602d3b75fa0f57426b853e2784070ac43bb8591
SimonFJ20 <simonfromjakobsen@gmail.com>
2021-02-22 14:45:37 +0100

    gitign

--- src/index.ts
+++ src/index.ts
@@ -8,9 +8,10 @@
 *
 *       License:    GPL-2.0
 *
-*       Author:     Simon From Jakobsen
-*       Email:      simonfromjakobsen@gmail.com
-*       GitHub:     SimonFJ20
+*       Authors:
+*       Simon From Jakobsen
+*           Email:      simonfromjakobsen@gmail.com
+*           GitHub:     SimonFJ20
 *
 *       Created:    22-02-2021
 *       Last Edit:  22-02-2021
